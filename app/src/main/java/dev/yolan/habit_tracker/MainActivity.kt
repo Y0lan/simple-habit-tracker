37b912e4c65d5c25d5e17c72c06aae00c1c0a622
noname <no@mail.com>
2021-01-23 00:24:28 +0100

    Initial commit

---
+++ app/src/main/java/dev/yolan/habit_tracker/MainActivity.kt
@@ -0,0 +1,15 @@
+package dev.yolan.habit_tracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        iv_icon.setImageResource(R.drawable.water)
+        tv_title.text = getString(R.string.drink_water)
+        tv_description.text = getString(R.string.drink_water_desc)
+    }
+}
